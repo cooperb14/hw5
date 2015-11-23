02ac25922b9aab46fdc9eed1a507222e90bf8658
Cooper Bates <cooperb14@gmail.com>
2015-11-23 10:01:11 -0500

    awaiting final testing and commenting

--- hw5/ReadMe.txt
+++ hw5/ReadMe.txt
@@ -2,13 +2,24 @@ Name: Cooper Bates
 UNI: cbb2153
 
 
-HW5_1 Percolate
+HW5_2 Percolate
 
 Description:
-This module percolation.py contains the functions outlined in assignment 5-1. For the read-in function I took in the first line of the file to store the array size, and read in the rest of the file at once using the read method. For write-out I used the write and write_lines methods to print the size and array independently. For make_sites I leveraged a method in the Numpy “random” library to produce an array where each element has a probablity p of being open. For vertical_flow I initially stored the size of the input array, then compressed the array into a 1-d list while simultaneously seeding a separate list of zeros of the same length. The remainder of the function iterates through both lists and alters the elements of the zeros array depending on the condition of its analogous elements in the initial array. By the end the zeros array has become the flow_matrix, and is shaped using the initial dimension value we stored. The Percolates function simply checks if the sum of the elements in the last row is greater than zero. 
+	This module percolation2.py contains the functions outlined in assignment 5-2. For the read_grid function the first line of the file is taken in to store the array size, and the rest of the file is read at once using the read method. For write_grid, write and write_lines methods are used to print the size and array independently.
+
+	Undirected_flow calls the flow_from function on each of the elements in the first row of the array. The flow_from function is a recursive function that uses the states of the flow and site vacancy matrix to follow an undirected flow path from each top-row element, and fill in the flow matrix accordingly.
+
+	The Percolates function simply checks if the sum of the elements in the last row is greater than zero. The make_sites function leverages a method in the Numpy “random” library to produce an array where each element has a probability p of being open. Show_perc leverages the colormap function of matplotlib.py and the combination of the flow and site arrays to produce a visual of the sites/flow.
+
+	Make_plot uses the matplotlib.py plot function to plot two lists against one another. The x list is filled with values of regular interval of between 0 and 1, with a step of .01 chosen for plotting speed, since accuracy is added by adaptive_plot helper function. At each step on the x axis, the percolation probability of a certain number of trials given by the user is calculated and appended to the y list.(The number of trials to calculate percolation probability is ultimately given by the user, however it is recommended that it be above 5000 for accuracy within a .01 range.) After the percolation probability is calculated at each step, a helper function called adaptive_plot is called. This function is a recursive function that checks the difference in y values between the current and last step, and if greater than a certain threshold, currently set to .075, then it will recursively estimate intermediary points to make plot smoother while still preserving plotting speed.
+
 
 Bugs:
-No bugs currently, that I have observed. Mind the additional Comments
+	No bugs currently, that I have observed. Since two functions are recursive, if their arguments are very low numbers it may exceed the computer’s Recursion Depth, producing an error. Mind the additional Comments
 
 Additional Comments:
-If a matrix is inputed where the dimensions are not square, the program will break. Also, if a text file name is inputed as an argument to the read_grid function, and the file does not exist in the given directory, an error will be thrown. 
+	If a matrix is inputed where the dimensions are not square, the program will break. Also, if a text file name is inputed as an argument to the read_grid function, and the file does not exist in the given directory, an error will be thrown.
+
+	With the make_plot and adaptive_plot function, the step variable in make_plot and the threshold_variable in adaptive_plot can be modified to create a graph more oriented toward plotting speed, or data point accuracy. The current way it is initialized is oriented more for plotting speed.
+
+In its current state the graph takes a little over two minutes to plot. 
--- hw5/percolation2.py
+++ hw5/percolation2.py
@@ -7,9 +7,69 @@ Percolation Module that exhibits functions defined by 1006 Hw5_2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# test imports
+import random
+import Lindsay as linz
+import time
 
 
+def read_grid(infile_name):
+    """Create a site vacancy matrix from a text file.
+
+    infile_name is the name (a string)  of the
+    text file to be read. The method should return 
+    the corresponding site vacancy matrix represented
+    as a numpy array
+    """
     
+    in_file = open(infile_name, 'r')
+    
+    size_n = int(in_file.readline())
+    ls = in_file.read().replace('\n',' ').split()
+    in_file.close()
+    
+    array = np.array([int(x) for x in ls])
+    array.shape = (size_n,size_n)
+    return array
+
+
+def write_grid(outfile_name,sites):
+    """Write a site vacancy matrix to a file.
+
+    outfile_name is a string that is the name of the
+    text file to write to. sites is a numpy array
+    representing the site vacany matrix to write
+    """
+    
+    out_file = open(outfile_name, 'w')        
+    ls = []
+    for n in range(len(sites)):
+        x = ' '.join([str(i) for i in sites[n]]) + '\n'
+        ls.append(x)
+    out_file.write(str(sites.shape[0]) + '\n')
+    out_file.writelines(ls)
+    out_file.close()
+
+
+def undirected_flow(sites):
+    """Returns a matrix of vacant/full sites (1=full, 0=vacant)
+
+    sites is a numpy array representing a site vacancy matrix. This 
+    function should return the corresponding flow matrix generated 
+    through directed percolation
+    """
+    
+    n = sites.shape[0]
+    
+    #create empty flow matrix
+    full = np.array([0] * n**2)
+    full.shape = (n,n)
+    
+    for j in range(n):
+        flow_from(sites, full, 0, j)
+    return full
+
+
 def flow_from(sites,full,i,j):
     """Adjusts the full array for flow from a single site
 
@@ -20,15 +80,29 @@ def flow_from(sites,full,i,j):
     if 0 <= i < n and 0 <= j < n:
         if sites[i,j] == 1 and full[i,j] != 1:
             full[i,j] = 1
-            #check right
-            flow_from(sites, full, i + 1, j)
-            #check left
-            flow_from(sites, full, i - 1, j)
-            #check up
-            flow_from(sites, full, i, j + 1)
-            #check below
-            flow_from(sites, full, i, j - 1)
             
+            #check right cell
+            flow_from(sites, full, i + 1, j)
+            
+            #check left cell
+            flow_from(sites, full, i - 1, j)
+            
+            #check up cell
+            flow_from(sites, full, i, j + 1)
+            
+            #check below cell
+            flow_from(sites, full, i, j - 1)
+
+
+def percolates(flow_matrix):
+    """Returns a boolean if the flow_matrix exhibits percolation
+    
+    flow_matrix is a numpy array representing a flow matrix
+    """
+    
+    return bool(sum(flow_matrix[flow_matrix.shape[0] - 1]) > 0)
+  
+
 def make_sites(n,p):
     """Returns an nxn site vacancy matrix
 
@@ -38,45 +112,22 @@ def make_sites(n,p):
     
     return np.random.binomial(1, p ,(n,n))
 
-def percolates(flow_matrix):
-    """Returns a boolean if the flow_matrix exhibits percolation
-    
-    flow_matrix is a numpy array representing a flow matrix
-    """
-    
-    return bool(sum(flow_matrix[flow_matrix.shape[0] - 1]) > 0)
-    
-    
-def undirected_flow(sites):
-    """Returns a matrix of vacant/full sites (1=full, 0=vacant)
-
-    sites is a numpy array representing a site vacancy matrix. This 
-    function should return the corresponding flow matrix generated 
-    through directed percolation
-    """
-    #create full/flow matrix
-    n = sites.shape[0]
-    
-    full = np.array([0] * n**2)
-    full.shape = (n,n)
-    
-    for j in range(n):
-        flow_from(sites, full, 0, j)
-    
-    return full
-
 
 def show_perc(sites):
     """Displays a matrix using three colors for vacant, blocked, full
     
     Used to visualize undirected flow on the matrix sites.
     """
+    
     x = undirected_flow(sites)
     y = sites
+    
+    #adds flow and site vacancy matricies to show overlap
     z = x + y
     plt.matshow(z)
     plt.show()
-    
+
+
 def make_plot(n,trials):
     """"generates and displays a graph of percolation p vs. vacancy p
 
@@ -86,10 +137,11 @@ def make_plot(n,trials):
     
     x = []
     y = []
-    p = .01
-    
+    last_y = 0
+    last_x = 0
+    p = 0
+    step = .01
     while p <= 1:
-        y.append(p)
         total = 0
         for counter in range(trials):
             a = make_sites(n,p)
@@ -97,19 +149,47 @@ def make_plot(n,trials):
             c = percolates(b)
             total += int(c)
         d = total/trials
-        x.append(d)
-        p += .01
+        j = []
+        i = []
+        adaptive_plot(d, p, j, i, last_y, last_x)
+        j.sort()
+        i.sort()
+        y.extend(j)
+        x.extend(i)
+        y.append(d)
+        x.append(p)
+        last_y = d
+        last_x = p
+        p += step
     plt.plot(x,y)
-        
+    plt.title('Graph of Percolation p vs. Site Vacancy p')
+    plt.ylabel('Percolation prob')
+    plt.xlabel('Site Vacancy prob')
+    plt.show()
+
+
+def adaptive_plot(y, x, j, i, last_y, last_x):
+    threshold = .075
+    if abs(y - last_y) > threshold:
+        y_middle = abs(y - last_y)/2
+        x_middle = abs(x - last_x)/2
+        j.append(y_middle)
+        i.append(x_middle)
+        adaptive_plot(y_middle, x_middle, j, i, last_y, last_x)
+        adaptive_plot(y, x, j, i, y_middle, x_middle)
+    
+
+
 def main():
 #    x = np.random.binomial(1, .35 ,(5,5))
 #   y = np.array([[0,0, 1, 0, 0],[1, 0, 1, 0, 1],[0, 0, 1, 0, 0],[1, 1, 1, 0, 0],[1, 0, 0, 0, 1]])
 #   plt.matshow(y)
 #   plt.show()
 #   show_perc(y)
+#    linz.make_plot(20,200)
+#    plt.show()
 
-# x = np.linspace(0,300,20)
-    make_plot(50,200)
-    
-    
+    x = time.time()
+    make_plot(10,5000)
+    print((time.time() - x)/60)
 main()
